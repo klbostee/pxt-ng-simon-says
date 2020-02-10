413cfab4d6eb0a5add3d5b7d08989344fc37e7f8
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-10 20:22:00 +0100

    First functional version

--- main.ts
+++ main.ts
@@ -1,7 +1,67 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `)
+function registerPress (value: number) {
+    if (ng.gameHasStarted()) {
+        presses.push(value)
+        showValue(value)
+    }
+}
+function showValue (value: number) {
+    if (value == 0) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+        ng.neopixels().showColor(neopixel.colors(NeoPixelColors.Yellow))
+        music.playTone(440, music.beat(BeatFraction.Half))
+    } else {
+        basic.showLeds(`
+            # # # # .
+            # . . . #
+            # # # # .
+            # . . . #
+            # # # # .
+            `)
+        ng.neopixels().showColor(neopixel.colors(NeoPixelColors.Blue))
+        music.playTone(494, music.beat(BeatFraction.Half))
+    }
+    basic.pause(100)
+    basic.clearScreen()
+    ng.neopixels().clear()
+    ng.neopixels().show()
+    basic.pause(delay)
+}
+input.onButtonPressed(Button.A, function () {
+    registerPress(0)
+})
+input.onButtonPressed(Button.B, function () {
+    registerPress(1)
+})
+let presses: number[] = []
+let delay = 0
+ng.startWithIcon(IconNames.EigthNote)
+if (ng.hardWasChosen()) {
+    delay = 100
+} else {
+    delay = 500
+}
+let sequence: number[] = []
+basic.forever(function () {
+    presses = []
+    sequence.push(Math.randomRange(0, 1))
+    for (let value of sequence) {
+        showValue(value)
+    }
+    while (presses.length < sequence.length) {
+        basic.pause(100)
+    }
+    for (let index = 0; index <= sequence.length; index++) {
+        if (presses[index] != sequence[index]) {
+            game.gameOver()
+        }
+    }
+    basic.pause(2000)
+    ng.incrementScore()
+    basic.pause(500)
+})
